b3d7c4ae2480f6af3fc705f1b5bf3b72733e58fb
Joseph Kichline <soolar121@gmail.com>
2021-12-25 14:08:47 -0800

    Display images for each favorite familiar to test

--- src/browser/App.tsx
+++ src/browser/App.tsx
@@ -1,9 +1,21 @@
 import * as React from "react";
+import {
+	BrowserFamiliar,
+	BrowserList,
+} from '../Guidelines'
+
+declare const familiars: BrowserList<BrowserFamiliar>;
 
 function App() {
+	const favoriteFamiliars = familiars.favorites.map((fam) => {
+		return (
+			<img src={`/images/itemimages/${fam.image}`} />
+		)
+	});
+
 	return (
 		<div id="chitterContainer">
-			Hello World...
+			{favoriteFamiliars}
 		</div>
 	);
 }
