4841d03e90b40c523e696482a2cc7418e5e6ab7a
Joseph Kichline <soolar121@gmail.com>
2022-01-04 17:21:14 -0800

    Add support for lack of familiar to FamIcon

--- src/browser/components/FamIcon.tsx
+++ src/browser/components/FamIcon.tsx
@@ -4,27 +4,37 @@ import { BrowserFamiliar } from '../../guidelines';
 import { pluralize } from '../../utils';
 
 interface FamIconArgs {
-	fam: BrowserFamiliar;
+	fam: BrowserFamiliar | undefined;
 	isBjorn?: boolean;
 }
 
 export default function FamIcon({ fam, isBjorn = false }: FamIconArgs) {
-	const dropsLeft = fam.dropsLimit - fam.dropsToday;
-	const hasDrops = !isBjorn && dropsLeft > 0;
-	const allDrops = hasDrops && fam.dropsToday === 0;
-	const dropName = fam.drop
-		? pluralize(fam.drop, dropsLeft)
-		: pluralize(fam.dropName, dropsLeft);
+	if (fam) {
+		const dropsLeft = fam.dropsLimit - fam.dropsToday;
+		const hasDrops = !isBjorn && dropsLeft > 0;
+		const allDrops = hasDrops && fam.dropsToday === 0;
+		const dropName = fam.drop
+			? pluralize(fam.drop, dropsLeft)
+			: pluralize(fam.dropName, dropsLeft);
 
-	return (
-		<span>
-			<Icon
-				image={fam.image}
-				title={`${fam.name} (the ${fam.weight}lb ${fam.type})${
-					dropName && hasDrops ? ` (${dropsLeft} ${dropName})` : ''
-				}`}
-				borderType={allDrops ? 'all-drops' : hasDrops ? 'has-drops' : 'normal'}
-			/>
-		</span>
-	);
+		return (
+			<span>
+				<Icon
+					image={fam.image}
+					title={`${fam.name} (the ${fam.weight}lb ${fam.type})${
+						dropName && hasDrops ? ` (${dropsLeft} ${dropName})` : ''
+					}`}
+					borderType={
+						allDrops ? 'all-drops' : hasDrops ? 'has-drops' : 'normal'
+					}
+				/>
+			</span>
+		);
+	} else {
+		return (
+			<span>
+				<Icon image="antianti.gif" title="You don't have a familiar with you" />
+			</span>
+		);
+	}
 }
