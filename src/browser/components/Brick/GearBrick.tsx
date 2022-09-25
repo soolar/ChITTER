a9820419d112439c715d5b7df9afefad0eb82410
soolar <soolar121@gmail.com>
2022-09-25 16:33:20 -0700

    use wrap instead of flex for gear brick

--- src/browser/components/Brick/GearBrick.tsx
+++ src/browser/components/Brick/GearBrick.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { Flex } from '@chakra-ui/react'
+import { Wrap, WrapItem } from '@chakra-ui/react'
 import { BrowserList, BrowserSlot } from '../../../guidelines'
 import ItemIcon from '../Icons/ItemIcon'
 import Brick from './Brick'
@@ -11,18 +11,23 @@ declare const slots: BrowserList<BrowserSlot>
 export default function GearBrick() {
 	return (
 		<Brick name="gear" header="Gear">
-			<Flex>
+			<Wrap justify="center" spacing="2px">
 				{slots.favorites.map((slot) => {
 					return (
-						<PickerLauncher
-							WrappedPicker={GearPicker}
-							pickerProps={{ slot: slot }}
-						>
-							<ItemIcon item={slot.equipped} tooltipPrefix={`${slot.name}: `} />
-						</PickerLauncher>
+						<WrapItem>
+							<PickerLauncher
+								WrappedPicker={GearPicker}
+								pickerProps={{ slot: slot }}
+							>
+								<ItemIcon
+									item={slot.equipped}
+									tooltipPrefix={`${slot.name}: `}
+								/>
+							</PickerLauncher>
+						</WrapItem>
 					)
 				})}
-			</Flex>
+			</Wrap>
 		</Brick>
 	)
 }
