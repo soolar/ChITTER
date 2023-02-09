60a7fa93ffa25b06f0c525c0b70d449d1426388d
soolar <soolar121@gmail.com>
2023-02-09 11:24:32 -0800

    handle the rare case where a modifier isn't a whole number

--- src/utils.ts
+++ src/utils.ts
@@ -63,19 +63,22 @@ const modShorthands: ModShorthand[] = [
 	[/Adventures/g, 'Adv'],
 	[/PvP Fights/g, 'Fites'],
 	// Combine regen min and max in to a range
-	[/((HP|MP|HP\/MP) Regen )Min: ([+-]?\d+), \1Max: \+?(-?\d+)/g, '$1$3-$4'],
+	[
+		/((HP|MP|HP\/MP) Regen )Min: ([+-]?[\d.]+), \1Max: \+?(-?[\d.]+)/g,
+		'$1$3-$4',
+	],
 	// simplify regen ranges that are identical
-	[/(\d+)-\1/g, '$1'],
+	[/([\d.]+)-\1/g, '$1'],
 	// Exp%: +5 -> Exp +5% (there is sometimes no : for some reason)
-	[/%:? ([+-]?\d+)/g, ' $1%'],
+	[/%:? ([+-]?[\d.]+)/g, ' $1%'],
 	// Wpn +5% looks too weird, extend it back to Weapon +5% for the drops case
 	[/Wpn Drop/g, 'Weapon Drop'],
 	// Item Drop: +5 -> Item +5% and such
-	[/Drop: ([+-]?\d+)/g, ' $1%'],
+	[/Drop: ([+-]?[\d.]+)/g, ' $1%'],
 	// remove colons
 	[/:/g, ''],
 	// Add missing + for some positives, but not ranges
-	[/ (\d+)([^-\d]|$)/g, ' +$1$2'],
+	[/ ([\d.]+)([^-\d.]|$)/g, ' +$1$2'],
 	// Reformat rollover effects. Hopefully this really is always at the end, and in this order
 	[
 		/Rollover Effect "([^"]+)", Rollover Effect Duration \+(\d+)/,
