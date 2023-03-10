b861d6aa84df3f4cd6e44b64028600eb599667cf
soolar <soolar121@gmail.com>
2023-03-09 16:12:40 -0800

    massive improvements to effect brick

--- src/browser/components/Brick/EffectsBrick.tsx
+++ src/browser/components/Brick/EffectsBrick.tsx
@@ -1,4 +1,4 @@
-import { Divider, GridItem, SimpleGrid, Text, VStack } from '@chakra-ui/react'
+import { Divider, Flex, HStack, Spacer, Text, VStack } from '@chakra-ui/react'
 import * as React from 'react'
 import { BrowserEffect, BrowserList } from '../../../guidelines'
 import { getExtraEffectInfo } from '../../effectHelpers'
@@ -10,7 +10,8 @@ declare const effects: BrowserList<BrowserEffect>
 export default function EffectsBrick() {
 	return (
 		<Brick name="Effects" header="Effects">
-			<SimpleGrid columns={4}>
+			<VStack>
+				<Divider />
 				{effects.active
 					.sort((eff1, eff2) => {
 						const turnsDiff = eff1.turnsActive - eff2.turnsActive
@@ -20,37 +21,36 @@ export default function EffectsBrick() {
 						const extraInfo = getExtraEffectInfo(eff)
 						return (
 							<>
-								<GridItem>
-									<EffectIcon effect={eff} />
-								</GridItem>
-								<GridItem>
-									<VStack spacing={0}>
-										<Text dangerouslySetInnerHTML={{ __html: eff.name }} />
-										<Text
-											className="desc-line"
-											dangerouslySetInnerHTML={{ __html: extraInfo.mods }}
-										/>
-									</VStack>
-								</GridItem>
-								<GridItem>
-									<Text>
-										{eff.turnsActive === 2147483647 ? (
-											<>&infin;</>
-										) : (
-											eff.turnsActive
-										)}
-									</Text>
-								</GridItem>
-								<GridItem>
-									<Text>^</Text>
-								</GridItem>
-								<GridItem colSpan={4}>
-									<Divider />
-								</GridItem>
+								<Flex className="chit-effect">
+									<HStack>
+										<EffectIcon effect={eff} />
+										<VStack spacing={0} className="chit-effect-description">
+											<Text dangerouslySetInnerHTML={{ __html: eff.name }} />
+											{extraInfo.mods.length > 0 && (
+												<Text
+													className="desc-line"
+													dangerouslySetInnerHTML={{ __html: extraInfo.mods }}
+												/>
+											)}
+										</VStack>
+									</HStack>
+									<Spacer />
+									<HStack>
+										<Text className="chit-effect-turns">
+											{eff.turnsActive === 2147483647 ? (
+												<>&infin;</>
+											) : (
+												eff.turnsActive
+											)}
+										</Text>
+										<Text className="chit-effect-extender">^</Text>
+									</HStack>
+								</Flex>
+								<Divider />
 							</>
 						)
 					})}
-			</SimpleGrid>
+			</VStack>
 		</Brick>
 	)
 }
