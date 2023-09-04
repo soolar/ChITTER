b22626cd87b27112a7e58db8ee08771c444d94ce
soolar <soolar121@gmail.com>
2023-09-04 16:16:46 -0700

    WHY WAS THIS

--- browser/src/components/Icons/ItemIcon.tsx
+++ browser/src/components/Icons/ItemIcon.tsx
@@ -69,7 +69,7 @@ export default function ItemIcon({
 						))}
 				</VStack>
 			}
-			borderType={'normal'}
+			borderType={extraInfo.borderType}
 			small={small}
 			onContextMenu={(ev) => {
 				if (showItemCallback) {
