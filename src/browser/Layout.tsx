ac318396c1ef353d48503c76048267687f56725f
soolar <soolar121@gmail.com>
2024-10-26 13:14:49 -0700

    GearBrick

--- src/browser/Layout.tsx
+++ src/browser/Layout.tsx
@@ -2,12 +2,14 @@ import { useContext } from 'react'
 import { Container } from '@chakra-ui/react'
 import { RefreshContext } from 'tome-kolmafia'
 import FamiliarBrick from './components/Brick/FamiliarBrick'
+import GearBrick from './components/Brick/GearBrick'
 
 export default function Layout() {
 	useContext(RefreshContext)
 
 	return (
 		<Container paddingX={0} fontSize="sm">
+			<GearBrick />
 			<FamiliarBrick />
 		</Container>
 	)
---
+++ src/browser/components/Brick/GearBrick.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import Brick from './Brick'
+import { Wrap, WrapItem } from '@chakra-ui/react'
+import PickerLauncher from '../Picker/PickerLauncher'
+import GearPicker from '../Picker/GearPicker'
+import ItemIcon from '../Icons/ItemIcon'
+import { equippedItem } from 'kolmafia'
+import { $slots } from 'libram'
+
+export default function GearBrick() {
+	const gearSlots = $slots`hat, back, shirt, weapon, off-hand, pants, acc1, acc2, acc3`
+	return (
+		<Brick name="gear" header="Gear">
+			<Wrap justify="center" spacing="2px">
+				{gearSlots.map((slot) => {
+					const equipped = equippedItem(slot)
+					return (
+						<WrapItem>
+							<PickerLauncher
+								WrappedPicker={GearPicker}
+								pickerProps={{ slot: slot }}
+							>
+								<ItemIcon
+									item={equipped}
+									tooltipPrefix={`${slot.identifierString}: `}
+								/>
+							</PickerLauncher>
+						</WrapItem>
+					)
+				})}
+			</Wrap>
+		</Brick>
+	)
+}
