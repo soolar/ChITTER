a32d56694941ce141ed9124f0fb01e8643d915b9
soolar <soolar121@gmail.com>
2025-03-15 19:25:59 -0700

    disable gear picking for invalid slots
    currently that just means the shirt slot without torso awareness but
    maybe there are some other circumstances for that I'm forgetting about?

--- src/browser/components/Brick/GearBrick.tsx
+++ src/browser/components/Brick/GearBrick.tsx
@@ -4,8 +4,9 @@ import { Wrap, WrapItem } from '@chakra-ui/react'
 import PickerLauncher from '../Picker/PickerLauncher'
 import GearPicker from '../Picker/GearPicker'
 import ItemIcon from '../Icons/ItemIcon'
-import { equippedItem } from 'kolmafia'
-import { $slots } from 'libram'
+import { equippedItem, haveSkill, print } from 'kolmafia'
+import { $skill, $slot, $slots } from 'libram'
+import ChitterIcon from '../Icons/ChitterIcon'
 
 export default function GearBrick() {
 	const gearSlots = $slots`hat, back, shirt, weapon, off-hand, pants, acc1, acc2, acc3`
@@ -14,17 +15,23 @@ export default function GearBrick() {
 			<Wrap justify="center" spacing="1px">
 				{gearSlots.map((slot) => {
 					const equipped = equippedItem(slot)
+					const canEquip =
+						slot !== $slot`shirt` || haveSkill($skill`Torso Awareness`)
 					return (
 						<WrapItem>
-							<PickerLauncher
-								WrappedPicker={GearPicker}
-								pickerProps={{ slot: slot }}
-							>
-								<ItemIcon
-									item={equipped}
-									tooltipPrefix={`${slot.identifierString}: `}
-								/>
-							</PickerLauncher>
+							{canEquip ? (
+								<PickerLauncher
+									WrappedPicker={GearPicker}
+									pickerProps={{ slot: slot }}
+								>
+									<ItemIcon
+										item={equipped}
+										tooltipPrefix={`${slot.identifierString}: `}
+									/>
+								</PickerLauncher>
+							) : (
+								<ChitterIcon image="antianti.gif" tooltip="Torso Unawareness" />
+							)}
 						</WrapItem>
 					)
 				})}
